d6ddb455979fb87e05115e6bb3779b6fc24f1b7b
lhuddlesto <lhuddlesto@gannett.com>
2020-05-16 13:16:55 -0400

    added optional port variable

--- src/server.js
+++ src/server.js
@@ -20,6 +20,6 @@ app.use(musicRouter);
 app.use(userRouter);
 app.use(passport.initialize());
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('Listening on port 5000');
 });
